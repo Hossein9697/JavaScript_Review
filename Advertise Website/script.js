99e470dc5c3dec4c3acb9d2464a8bb9a1ff36344
Hossein <hheyatzadeh@gmail.com>
2023-08-10 19:48:32 +0330

    Show active tab

--- Advertise Website/script.js
+++ Advertise Website/script.js
@@ -8,6 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -74,4 +77,19 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document
       .querySelector(e.target.getAttribute('href'))
       .scrollIntoView({ behavior: 'smooth' });
+});
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(tabContent =>
+    tabContent.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
